c1f9c39f0d790a9f1905502d0b53282b71da7e6c
rwspicer <rwspicer@alaska.edu>
2015-10-01 14:00:07 -0800

    fixed bugs with typos

--- model/residential_buildings.py
+++ model/residential_buildings.py
@@ -35,34 +35,45 @@ class ResidentialBuildings(AnnualSavings):
     
     def run (self):
         """ Function doc """
-        self.calc_init_hh()
+        self.calc_init_HH()
         self.calc_opportunity_values()
         self.calc_init_HF_use()
     
-    def calc_init_hh (self):
+    def calc_init_HH (self):
         """ Function doc """
-        val = self.forecast.get_population(self.start_year,self.start_year+1)[0]
-        self.init_hh = int(round(self.cd["households"]*\
+        val = self.forecast.get_population(self.start_year)
+        
+        
+        self.init_HH = int(round(self.cd["households"]*\
                         (val / self.cd["population"])))
         
     def calc_opportunity_values (self):
         """ Function doc """
         rd = self.cd["res_model_data"]
-        self.opportunity_hh = self.init_hh -rd["BEES_number"] -rd["post_number"]
-        self.opportunity_savings = self.opportunity_hh * \
+        ##  #HH
+        self.opportunity_HH = self.init_HH -rd["BEES_number"] -rd["post_number"]
+        ## cell L15 = K15*S8*(P8*O8-6000*0.00341)/0.138
+        ## gal = #HH*(%)*((MMBtu/sqft)*sqft-(#*#))/# 
+        ## gal = #*(MMBtu - (#*#))/#  ???
+        self.opportunity_savings = self.opportunity_HH * \
                                    rd["post_avg_EUI_reduction"] * \
                                    (rd["pre_avg_EUI"] * rd["pre_avg_area"] -\
                                     6000*0.00341) / 0.138 # numbers ??
+        ## % as decimal 
         self.percent_savigns = rd["opportunity_total_percent_community_savings"]
         
         
     def calc_init_HF_use (self):
         """ Function doc """
         rd = self.cd["res_model_data"]
+        ## cell J15 = (M8+U8+K15*O8*P8-I8*6000*0.00341)/0.138
+        ##  gal = (MMBtu + MMBtu + (#HH*sqft*MMBTU/sqft) - (#HH*#*#))/#  
+        ##  gal = (MMBtu + (MMBtu) - (#HH*#*#))/#
+        ##  gal = (MMBTU - (#HH*#*#))/#  ????  
         self.init_HF_use = (rd["BEES_total_consumption"] + \
                                      rd["post_total_consumption"] + \
-                    self.opportunity_hh*rd["pre_avg_area"]*rd["pre_avg_EUI"] - \
-                        self.init_hh*6000*0.00341) / 0.138
+                    self.opportunity_HH*rd["pre_avg_area"]*rd["pre_avg_EUI"] - \
+                        self.init_HH*6000*0.00341) / 0.138
         
     def calc_capital_costs (self):
         """ Function doc """
