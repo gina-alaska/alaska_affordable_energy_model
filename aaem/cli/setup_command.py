28010e951c89a8655b0943389b6b6f6f755c847b
rwspicer <rwspicer@alaska.edu>
2016-01-14 13:35:39 -0900

    working setup -- dosen't do much

--- aaem/cli/setup_command.py
+++ aaem/cli/setup_command.py
@@ -1,39 +1,44 @@
 import pycommand
 import sys
 import os.path
+from aaem import driver
 
 class SetupCommand(pycommand.CommandBase):
     usagestr = 'usage: setup [directory]'
     description = 'Set up directory for running AAEM Models'
 
     optionList = (
-        ('name', ('-n', "<name>", "name of model")),
-        
+           ('path', ('n', "<name>", "name of model")),
+        #~ ('name', ('n', "<name>", "name of model")),
     )
 
     def run(self):
         
-        print('Python version ' + sys.version.split()[0])
-        print('Path: ' + os.getcwd())
+        #~ print('Python version ' + sys.version.split()[0]))
+        
         path = os.getcwd()
-        
-        
-        
-        
-        print self.flags
-        
-        if self.flags.name:
-            print self.flags.name
-            name = '_' + self.flags.name
-        else:
-            name = ""
+        if self.flags.path:
+            path = os.path.abspath(self.flags.path)
+            
+        name = ""
+        #~ if self.flags.name:
+            #~ name = '_' + self.flags.name
+            
         
         
         
         try:
-            os.makedirs(os.path.join(path,"model" + name))
+            model_root = os.path.join(path,"model" + name)
+            os.makedirs(os.path.join(model_root, "raw_data"))
+            os.makedirs(os.path.join(model_root, "input_data"))
+            os.makedirs(os.path.join(model_root, "config"))
+            os.makedirs(os.path.join(model_root, "results"))
         except OSError:
             pass
+
+    coms = ["Bethel","Craig","Dillingham","Haines","Manley Hot Springs","Nome",
+            "Sand Point","Sitka","Tok","Yakutat","Valdez"]
+        driver.setup_multi([coms])
 
 if __name__ == '__main__':
     # Shortcut for reading from sys.argv[1:] and sys.exit(status)
