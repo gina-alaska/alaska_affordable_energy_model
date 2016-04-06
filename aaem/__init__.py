b6373e0abbcd3d3c222037f822126264ffa22c71
rwspicer <rwspicer@alaska.edu>
2016-04-06 13:05:57 -0800

    added version #

--- aaem/__init__.py
+++ aaem/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.7.4'
+__version__ = '0.8.0'
 __url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model'
-__download_url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model/releases/tag/v0.7.4'
+__download_url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model/releases/tag/v0.8.0'
