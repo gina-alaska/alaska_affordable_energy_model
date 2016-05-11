30f56b5ec6d760c8eb2ce4300e5f13df99a7fd0c
rwspicer <rwspicer@alaska.edu>
2016-05-11 08:43:00 -0800

    basic wind comp, does nothing

--- aaem/components/__init__.py
+++ aaem/components/__init__.py
@@ -2,5 +2,6 @@ comp_lib = {
     "residential buildings": "residential_buildings",
     "non-residential buildings": "community_buildings",
     "water wastewater": "wastewater",
+    "wind power": "wind_power",
         }
 
