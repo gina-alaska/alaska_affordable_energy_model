eac8c7049f4bfa14a2dcd21de9607fd82b18d4dc
rwspicer <rwspicer@alaska.edu>
2016-06-08 13:43:49 -0800

    biomass base

--- aaem/__init__.py
+++ aaem/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.14.3+solar_adg'
+__version__ = '0.14.3+biomass_base'
 __url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model'
 __download_url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model/releases/tag/v0.14.3'
