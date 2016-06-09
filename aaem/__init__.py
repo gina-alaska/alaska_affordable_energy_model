8caaedb5cd81aef86da6b0838459699f31dd3e63
rwspicer <rwspicer@alaska.edu>
2016-06-09 08:17:44 -0800

    branch version

--- aaem/__init__.py
+++ aaem/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.14.4'
+__version__ = '0.14.4+biomass-all'
 __url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model'
 __download_url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model/releases/tag/v0.14.4'
