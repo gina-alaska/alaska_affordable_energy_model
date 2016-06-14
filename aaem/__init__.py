ec7c01de2b7e710390cc70da0d203a68f91f07b6
rwspicer <rwspicer@alaska.edu>
2016-06-14 14:59:18 -0800

    added in wind to exitstin RE equation

--- aaem/__init__.py
+++ aaem/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.14.5'
+__version__ = '0.14.5+solar_cap'
 __url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model'
 __download_url__ = 'https://github.com/gina-alaska/alaska_affordable_energy_model/releases/tag/v0.14.5'
