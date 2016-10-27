b5d488d8daade6058f391c6598b22122df7cf213
rwspicer <rwspicer@alaska.edu>
2016-10-27 13:27:17 -0800

    added a comment

--- aaem/components/wind_power/component.py
+++ aaem/components/wind_power/component.py
@@ -198,6 +198,8 @@ class WindPower(AnnualSavings):
         offset = self.average_load*\
                 self.comp_specs['percent generation to offset']
         #~ print self.forecast.generation_by_type['generation hydro'].sum()
+        
+        # removed on purpose
         #~ hydro = \
             #~ self.forecast.generation_by_type['generation hydro'].fillna(0).sum()
         #~ if hydro > 0:
