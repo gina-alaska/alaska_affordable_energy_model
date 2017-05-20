8095312f90a3b8c7e4c4bbd21c8f4da194fd4833
rwspicer <rwspicer@alaska.edu>
2017-05-19 16:13:10 -0800

    reenabled the population preprocessing

--- aaem/preprocessor2.py
+++ aaem/preprocessor2.py
@@ -555,12 +555,11 @@ class Preprocessor (object):
         
         ids = self.GNIS_ids
         
-        #~ if not self.process_intertie:
-        
-        if self.intertie_status == 'child':
-            ids = [ids[1]]
-        else:
-            ids = [ids[0]]
+        if not self.process_intertie:
+            if self.intertie_status == 'child':
+                ids = [ids[1]]
+            else:
+                ids = [ids[0]]
             
             
         if 'population_ids_to_use' in kwargs:
