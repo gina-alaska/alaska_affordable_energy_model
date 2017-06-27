59c615ce3da6c602e2cb74b9f0619e7e0b63c77a
Jen Delamere <jsdelamere@alaska.edu>
2017-06-27 14:38:37 -0800

    Updating definitions

--- aaem/components/wind_power/config.py
+++ aaem/components/wind_power/config.py
@@ -1,24 +1,10 @@
 """
-Wind Power Configuration 
+Wind Power Configuration
 ------------------------
-    
+
     Contains Transmission configuration info for community
     data yaml file, and other set-up requirements
-    
-    **Unique Configuration keys**
-    
-        'default distance to resource'
-        'average load limit'
-        'percent generation to offset'
-        'resource data'
-        'secondary load'
-        'secondary load cost'
-        'road needed for transmission line'
-        'est. transmission line cost'
-        'costs'
-        'percent o&m'
-        'percent heat recovered'
-    
+
 """
 from pandas import DataFrame
 from aaem.components import definitions
@@ -33,8 +19,8 @@ order = [
     'start year',
     'average load limit',
     'percent generation to offset',
-    
-    
+
+
     'name',
     'source',
     'notes',
@@ -45,13 +31,13 @@ order = [
     'proposed generation',
     'distance to resource',
     'transmission capital cost',
-    
+
     'wind class',
     'capacity factor',
     'percent heat recovered',
     'secondary load',
-    
-    
+
+
     'secondary load cost',
     'percent o&m',
     'estimated costs',
@@ -61,11 +47,11 @@ order = [
 structure = {
     'Wind Power': {
         'enabled': bool,
-        'lifetime': int, 
+        'lifetime': int,
         'start year': int,
         'average load limit': float,
         'percent generation to offset': float,
-        
+
         'name': str,
         'source': str,
         'notes': str,
@@ -74,17 +60,17 @@ structure = {
         'generation capital cost': [float, str],
         'operational costs': [float, str],
         'proposed generation': [float, str],
-        'distance to resource': float, 
+        'distance to resource': float,
         'transmission capital cost': [float, str],
-        
-        
+
+
         'wind class': float,
         'capacity factor': float,
         'percent heat recovered': float,
         'secondary load': bool,
-        
-        
-        'secondary load cost': float, # 
+
+
+        'secondary load cost': float, #
         'percent o&m': float,
         'estimated costs': DataFrame,
         'est. transmission line cost': float,
@@ -92,37 +78,34 @@ structure = {
 }
 
 comments = {
-    'enabled': 'bool',
-    'lifetime': 'int', 
-    'start year': 'int',
-    'average load limit': 'float',
-    'percent generation to offset': 'float',
-    
-    
-    'name': 'str',
-    'source': 'str',
-    'notes': 'str',
-    'phase': 'str',
+    'enabled': definitions.ENABLED,
+    'start year': definitions.START_YEAR_WITH_TYPE,
+    'lifetime': definitions.LIFETIME,
+    'average load limit': '[float]',
+    'percent generation to offset': '[float]',
+
+    'name': definitions.NAME,
+    'source': definitions.SOURCE,
+    'notes': definitions.NOTES_WITH_TYPE,
+    'phase': definitions.PHASE_WITH_TYPE,
     'proposed capacity': 'f[float, str]',
     'generation capital cost': 'f[float, str]',
     'operational costs': 'f[float, str]',
     'proposed generation': 'f[float, str]',
-    'distance to resource': 'float', 
+    'distance to resource': '[float]',
     'transmission capital cost': 'f[float, str]',
-    
-    
-    'wind class': 'float',
-    'capacity factor': 'float',
-    'percent heat recovered': 'float',
-    'secondary load': 'bool',
-    
-    
-    'secondary load cost': 'float', # 
-    'percent o&m': 'float',
-    'estimated costs': 'DataFrame',
-    'est. transmission line cost': 'float',
+
+    'wind class': '[float]',
+    'capacity factor': '[float]',
+    'percent heat recovered': '[float]',
+    'secondary load': '[bool]',
+
+
+    'secondary load cost': '[float]', #
+    'percent o&m': '[float]',
+    'estimated costs': '[DataFrame]',
+    'est. transmission line cost': '[float]',
 }
 
 ## list of prerequisites for module
 prereq_comps = []
-
