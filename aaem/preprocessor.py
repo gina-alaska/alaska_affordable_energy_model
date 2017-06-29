fix weighted average

aaem/preprocessor.py
@@ -796,7 +796,7 @@ class Preprocessor (object):
         #~ print len(data)
         ## get last year with full data
         last_year = data["year"].max()
-        while len(data[data["year"] == last_year])%12 != 0:
+        while len(data[data["year"] == last_year].ix[coms[0]])%12 != 0:
             last_year -= 1
         
         ## get last years electric fuel cost
@@ -807,8 +807,8 @@ class Preprocessor (object):
         
         elec_fuel_cost = 0
         res_nonPCE_price = 0
-        total_sold = data[data["year"] == last_year][cols].sum(axis = 1).mean()
-        
+        total_sold = data[data["year"] == last_year][cols].sum(axis = 1).sum()
+        w_t = 0
         for com in  coms:
             
             com_elec_fuel_cost = (
@@ -817,17 +817,16 @@ class Preprocessor (object):
             ).mean()
             weight = \
                 data[data["year"] == last_year].\
-                ix[com][cols].sum(axis = 1).mean()/\
+                ix[com][cols].sum(axis = 1).sum()/\
                 total_sold
             elec_fuel_cost += (com_elec_fuel_cost * weight)
             
             res_pce = \
                 data[data["year"] == last_year].ix[com]\
                 ["residential_rate"].mean()
-        
+            #~ print weight
             res_nonPCE_price += (res_pce * weight)
-        
-        #~ print elec_fuel_cost
+            w_t+=weight
         
         if np.isnan(elec_fuel_cost):
             elec_fuel_cost = 0.0
@@ -1028,13 +1027,15 @@ class Preprocessor (object):
             #~ print rate_data
             for com in  coms:
                 #~ print rate_data[
-                weight = rate_data[rate_data['community'] == com][sales_cols].sum().sum()/total_sold
+                weight = \
+                    rate_data[rate_data['community'] == com][sales_cols]\
+                .sum().sum()/total_sold
                 
                 res_pce = \
                     rate_data[rate_data['community'] == com]\
                     ["residential_rate"].mean()
                 res_rate += (res_pce * weight)
-                
+            #~ print 'cons', year, res_rate 
             years_data['residential rate'] = res_rate
                 
             years_data["diesel price"] = data.ix[year]["fuel_price"].mean()