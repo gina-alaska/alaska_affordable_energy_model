96fed0137152985ce280ea37e0affec131e3087f
rwspicer <rwspicer@alaska.edu>
2017-06-30 15:43:06 -0800

    add '_''s

--- aaem/cli/default_cases.py
+++ aaem/cli/default_cases.py
@@ -12,6 +12,6 @@ __DEV_COMS__ =  ["Adak","Bethel","Craig","Dillingham",
 
 ## for run command _intertie was added to interties
 __DEV_COMS_RUN__ =  ["Adak", "Bethel_intertie","Craig_intertie","Dillingham_intertie",
-                     "Haines_intertie","Manley Hot Springs",
-                     "Nome","Sand Point","Sitka","Tok_intertie",
+                     "Haines_intertie","Manley_Hot_Springs",
+                     "Nome","Sand_Point","Sitka","Tok_intertie",
                      "Yakutat","Valdez"]
